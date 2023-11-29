800b4ed693b665f8179ee8a9d70fc71e4ab3c0f8
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-11-29 08:11:52 -0800

    add view last

--- run.sh
+++ run.sh
@@ -53,10 +53,6 @@ if [ "$PULL_BCONLINE_BILLING_RECORD" == true ]; then
   pull_file_from_ocp "BCONLINE_BILLING_RECORD_output.sql" "data-yesterday" "COLIN"
 fi
 
-if [ "$CREATE_VIEWS" == true ]; then
-  gcloud --quiet sql import sql $GCP_SQL_INSTANCE "gs://${DB_BUCKET}/views/views.sql" --database=$DB_NAME --user=$DB_USER
-fi
-
 if [ "$LOAD_PAY" == true ] || [ "$LOAD_COLIN_DELTAS" == true ] || [ "$LOAD_COLIN_BASE" == true ]; then
   echo "connecting to openshift"
   oc login --server=$OC_SERVER --token=$OC_TOKEN
@@ -212,6 +208,10 @@ if [ "$LOAD_CAS" == true ]; then
       gcloud sql operations list --instance=$GCP_SQL_INSTANCE --filter='NOT status:done' --format='value(name)' | xargs -r gcloud sql operations wait --timeout=unlimited
     fi
   done
+fi
+
+if [ "$CREATE_VIEWS" == true ]; then
+  gcloud --quiet sql import sql $GCP_SQL_INSTANCE "gs://${DB_BUCKET}/views/views.sql" --database=$DB_NAME --user=$DB_USER
 fi
 
 touch readonly.sql
